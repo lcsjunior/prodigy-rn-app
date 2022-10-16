5faa0ae7d44979934a025cb48f4c1a66796a1aed
Luiz Carlos Jr <lcsjuniorsi@gmail.com>
2022-10-16 11:11:58 -0300

    docs: fix cfgs

--- app.config.js
+++ app.config.js
@@ -2,8 +2,8 @@ module.exports = ({ config }) => {
   return {
     ...config,
     extra: {
-      baseApiUrl: 'http://api.prodigyio.com:9001',
-      thingSpeakApiUrl: 'https://api.thingspeak.com',
+      baseApiUrl: process.env.PRODIGY_API_URL,
+      thingSpeakApiUrl: process.env.THINGSPEAK_API_URL,
     },
   };
 };
